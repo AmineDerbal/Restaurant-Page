b6f2448d60896369733455adeeedc8f6560a9dcb
Kyu931 <aminedoran31@gmail.com>
2022-04-14 14:19:23 +0100

    update function clear in clear.js and create file contact.js

--- src/clear.js
+++ src/clear.js
@@ -1,15 +1,19 @@
 const clear = () =>{
   const container = document.querySelector('.container');
 
-  if(container.childElementCount > 0){
-    let firstElement = container.firstElementChild;
-    while(firstElement){
-      firstElement.remove();
-      firstElement = container.firstElementChild;
-      
+  if (container){
+    
+    
+    if(container.childElementCount > 0){
+      let firstElement = container.firstElementChild;
+      while(firstElement){
+        firstElement.remove();
+        firstElement = container.firstElementChild;
+        
+      }
     }
+    container.remove();
   }
-  container.remove();
+  
 }
-
 export default clear
---
+++ src/contact.js
@@ -0,0 +1,8 @@
+const contact = () => {
+
+  let content = document.getElementById('content');
+  const container = document.createElement('div');
+container.classList.add('container');
+
+content.appendChild(container);
+}
